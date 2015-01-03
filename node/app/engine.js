ecf861b2bcbfc029d2638575460704da06ec172b
Farhankhan Daya <fkdaya@gmail.com>
2015-01-03 02:53:34 -0600

    Next step in a more oop structure. Still needs adjustments

--- node/app/engine.js
+++ node/app/engine.js
@@ -1,28 +1,36 @@
 
 exports = module.exports = function engine(data) {
-    this.playerList = {}; 
-    this.world = require('./entities/world');
+    this.players = {}; 
+    this.zones = {
+        'zoneName': zoneobj
+    };
+    this.zoneMap = {
+        'zoneName': {
+            'up': zone above it,
+            'right': zone to the right,
+            'down': zone below this one,
+            'left': zone to the left of this one
+        }
+    };
+
 };
-
-Things the engine needs to handle.
-
-- Create World object. Each server will have it's own engine running with it's own world for scalability reasons
-- Do I actually need a World object? Shouldn't the engine object actually do everything the world object is supposed to do?
-    - What do I split between the engine object and the world object. Discuss with Keith.
-
-
 
 // The server will call this whenever a player logs in to the game. Add player to global array of logged in players
 module.exports.loadPlayer = function loadPlayer(username) {
+    // Check if player with username is already in player list
+    // if player is not already in player list, load player from db
+    // if player is not in db, create new player and save once to db
+    // put loaded/created player in player list
+
     player list add(db.queryDB.query('get player by username'))
-
-
 
     playerList[username] = Playerobject;
 }
 
 // The server will call this whenever a player logs out/leaves the game. Save player data to the server and remove from array of logged in players
 module.exports.unloadPlayer = function unloadPlayer(username) {
+    // delete player from player list
+    // save player to database
 
     var saveThis = playerList[username];
     delete playerList[username];
--- node/app/entities/player.js
+++ node/app/entities/player.js
@@ -1,12 +1,11 @@
-exports = module.exports = function Player(username) {
-    
-    var this.username = ,   // Player display name. Unique identifier
-    this.gender = ,
+function Player(username, gender, inventory, party, PC, pokedex, pokenav, card, settings) {
+    this.username = username;       // Player display name. Unique identifier
+    this.gender = gender;
     this.pos = {            // Player positional data within the world
-        zone: 
-        x: 
-        y: 
-    },
+        'zone': 
+        'x': 
+        'y': 
+    };
     this.inventory = {      // Player inventory
         'items': [],
         'pokeballs': [],
@@ -14,18 +13,15 @@ exports = module.exports = function Player(username) {
         'berries': [],
         'key items': [],
         'medicine': [],
-    },
-    this.party = [],        // Current Pokemon the player has with them. The limit is 6.
-    this.PC = {},           // The PC of the player
-    this.pokedex = {},
-    this.pokenav = {},
-    this.card = {},         // Trainer Card - Used to keep track of player gym badges
-    this.settings = {}      // Options editable by player such as text speed, battle scene options, sound, etc.
+    };
+    this.party = [];        // Current Pokemon the player has with them. The limit is 6.
+    this.PC = {};           // The PC of the player
+    this.pokedex = {};
+    this.pokenav = {};
+    this.card = {};         // Trainer Card - Used to keep track of player gym badges
+    this.settings = {};     // Options editable by player such as text speed, battle scene options, sound, etc.
 
-    /*
-    
-        Things a player object needs to encompass
-
-
-    */
 }
+
+
+exports = module.exports = new Player(username, gender, inventory, party, PC, pokedex, pokenav, card, settings);
