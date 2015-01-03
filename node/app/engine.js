9b1663b2f46117e9ff18b0aea9d06ad14961a20f
Farhankhan Daya <fkdaya@gmail.com>
2015-01-03 03:53:27 -0600

    Did some cleaning up for sanity. Need to establish the use of prototyping

--- node/app/engine.js
+++ node/app/engine.js
@@ -1,33 +1,42 @@
-
-exports = module.exports = function engine(data) {
-    this.players = {}; 
+/*
+ * The heart of the game backend. Contains the world representation and performs
+ * all operations on the world.
+ */
+exports = module.exports = function engine() { 
+    this.players = {};
     this.zones = {
         'zoneName': zoneobj
     };
     this.zoneMap = {
         'zoneName': {
             'up': zone above it,
-            'right': zone to the right,
             'down': zone below this one,
             'left': zone to the left of this one
+            'right': zone to the right,
         }
     };
-
 };
 
-// The server will call this whenever a player logs in to the game. Add player to global array of logged in players
+/*
+ * Called whenever a player logs in to the game. Add player to global array of logged in players.
+ * @username The username of the player to be loaded in
+ */
 module.exports.loadPlayer = function loadPlayer(username) {
-    // Check if player with username is already in player list
-    // if player is not already in player list, load player from db
-    // if player is not in db, create new player and save once to db
-    // put loaded/created player in player list
+    if(!players[username]) {
+        // Load player
+        var newPlayer = db.queryDB.query('get player by username');
+        if(!newPlayer)
+            newPlayer = new Player();
 
-    player list add(db.queryDB.query('get player by username'))
-
-    playerList[username] = Playerobject;
+        players[username] = newPlayer;
+    }
 }
 
-// The server will call this whenever a player logs out/leaves the game. Save player data to the server and remove from array of logged in players
+/*
+ * Called whenever a player logs out/leaves the game. Saves player data to the server and 
+ * remove player from players array
+ * @username The username of the player to be unloaded
+ */
 module.exports.unloadPlayer = function unloadPlayer(username) {
     // delete player from player list
     // save player to database
--- node/app/entities/Player.js
+++ node/app/entities/Player.js
@@ -1,14 +1,15 @@
 /*
  * Each player in the game is represented with a player object.
- * @inventory is an inventory object
+ * @inventory is an Inventory object
+ * @settings is a Settings object
  */
-function Player(username, gender, inventory, party, PC, pokedex, pokenav, card, settings) {
+function Player(username, gender, zone, x, y, inventory, party, PC, pokedex, pokenav, card, settings) {
     this.username = username;       // Player display name. Unique identifier
     this.gender = gender;
     this.pos = {            // Player positional data within the world
-        'zone': 
-        'x': 
-        'y': 
+        'zone': zone,
+        'x': x,
+        'y': y
     };
     this.inventory = inventory;      // Player inventory
     this.party = [];        // Current Pokemon the player has with them. The limit is 6.
@@ -16,8 +17,8 @@ function Player(username, gender, inventory, party, PC, pokedex, pokenav, card, 
     this.pokedex = {};
     this.pokenav = {};
     this.card = {};         // Trainer Card - Used to keep track of player gym badges
-    this.settings = {};     // Options editable by player such as text speed, battle scene options, sound, etc.
+    this.settings = settings;     // Options editable by player such as text speed, battle scene options, sound, etc.
 }
 
 
-exports = module.exports = new Player(username, gender, inventory, party, PC, pokedex, pokenav, card, settings);
+exports = module.exports = new Player(username, gender, zone, x, y, inventory, party, PC, pokedex, pokenav, card, settings);
