40268c7f70059807efcf3dfc01f9bd02c27ee96b
Keith Armstrong <keith1015@gmail.com>
2015-01-05 15:40:52 -0600

    Removed backup cmds

--- node/console/backup/history.js
+++
@@ -1,9 +0,0 @@
-module.exports = function() {
-    retval = false;
-    console.log();
-    var a = 0;
-    history.forEach(function (item) {
-        console.log(++a+".\t"+item);
-    });
-    return false;
-};
