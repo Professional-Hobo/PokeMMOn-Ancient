91893decc970174c4f43c533bce6da39a955f111
Keith Armstrong <keith1015@gmail.com>
2015-01-13 11:20:45 -0600

    fetchAssets is now setup script and now also performs server's npm install

---
+++ setup.sh
@@ -0,0 +1,17 @@
+echo "=== Pokemon Sprites ==="
+echo "Creating Pokemon sprites directory..."
+mkdir client/assets/sprites/pokemon
+cd client/assets/sprites/pokemon
+echo "Downloading Pokemon sprites..."
+curl -L https://www.dropbox.com/s/sy9pcrk3vpzogpt/sprites.tar.gz?dl=1 -o sprites.tar.gz
+echo "Extracting Pokemon sprites..."
+tar xf sprites.tar.gz
+echo "Removing compressed file..."
+rm sprites.tar.gz
+echo "Done with Pokemon Sprites"
+echo "=== npm install =="
+cd ../../../..
+sleep 2
+echo "Setting up server's npm install for server directory"
+(cd server && npm install)
+echo "Done with server's npm install"
